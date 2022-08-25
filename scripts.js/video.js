f6da118dfba4199a7a3af41b70f65ceddd514a7e
rohit1620 <malavrohit1620@gmail.com>
2022-08-25 19:57:47 +0530

    day-3 video work finish

--- scripts.js/video.js
+++ scripts.js/video.js
@@ -1,5 +1,5 @@
 
-{/* <img src="https://www.cricbuzz.com/a/img/v1/595x396/i1/c${241290}/jonny-bairstow-hammered-a-92-b.jpg" alt=""></img> */}
+// {/* <img src="https://www.cricbuzz.com/a/img/v1/595x396/i1/c${241290}/jonny-bairstow-hammered-a-92-b.jpg" alt=""></img> */}
 
 let getData=async()=>{
     let res=await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=%20cricket%20video%20Aug%2C2022&key=${"AIzaSyBJOP_SWdYFIkJgZ1PvQOY4YqhUqkPG1Ug"}`);
@@ -7,8 +7,8 @@ let getData=async()=>{
     console.log(res)
     append(res.items)
 }
-  getData()
-
+    getData()
+  // `https://www.youtube.com/embed/${videoId}`
    /* data.forEach((el) => { el.id.videoId/ el.snippet.title */
     let append=(data)=>{
         console.log(data)
@@ -18,9 +18,9 @@ let div2 = document.createElement("div");
 let div = document.createElement("div");
 let p = document.createElement("p");
 p.innerText=description;
-let iframe = document.createElement("iframe");
-iframe.src = `https://www.youtube.com/embed/${videoId}`;
-iframe.allow = "fullscreen";
+let iframe = document.createElement("img");
+iframe.src =thumbnails.medium.url;
+// iframe.allow = "fullscreen";
 let h3 = document.createElement("h3");
 h3.innerText = title;
 
@@ -45,3 +45,22 @@ document.getElementById("cont").append(div)
 
 });
 };
+let videos=JSON.parse(localStorage.getItem("video"))
+
+console.log(videos)
+// let right=()=>{
+let iframe = document.createElement("iframe");
+ iframe.src =`https://www.youtube.com/embed/${videos.videoId}`
+ iframe.allow = "fullscreen";
+ let h4 = document.createElement("h4");
+h4.innerText = videos.title;
+document.getElementById("iframe").append(iframe,h4)
+
+// }
+
+let playVideo=(video)=>{
+   localStorage.setItem("video",JSON.stringify(video))
+  window.location.href="video.html"
+}
+
+
