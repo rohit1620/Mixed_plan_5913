0e788208d122c1ca888ea4dd690bb7ebee667533
sumansauravmay <sumansauravmay12345@gmail.com>
2022-08-27 02:52:05 +0530

    day-4 by Suman Saurav

---
+++ component/footer.js
@@ -0,0 +1,22 @@
+function footer(){
+    return ` <div id="div_1">
+    <img src="https://www.cricket.com/static/svgs/cricket-logo.svg">
+     <p>Cricket like never before</p>
+     </div>
+ 
+   <div id="div_2">
+       <a href="https:"#">Term of use</a>
+       <a href="https:"#">Privacy Policy</a>
+       <a href="https:"#">Cookies Policy</a>
+   </div>
+   <div id="div_3">
+       <div id="flex">
+       <h5>follow us on</h5>
+       <img src="https://www.cricket.com/svgs/facebook.svg">
+       <img src="https://www.cricket.com/svgs/twitter.svg">
+       <img src="https://www.cricket.com/svgs/linkedin.svg">
+       </div>
+       <p>@2020 Cricket.com | All right reserved</p>
+   </div>`;
+}
+export default footer;
---
+++ fantasy.js
@@ -0,0 +1,7 @@
+import navbar from "./component/navbar.js";
+document.querySelector("#navbar").innerHTML=navbar();
+
+import footer from "./component/footer.js";
+document.querySelector("#footer").innerHTML=footer();
+
+
